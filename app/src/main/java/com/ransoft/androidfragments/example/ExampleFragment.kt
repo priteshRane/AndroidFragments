269d82b07f7e96c85845c4a61bf753ccb732235a
Pritesh_PC <priteshrane01@gmail.com>
2020-06-12 12:33:49 +0530

    Example fragment is added

---
+++ app/src/main/java/com/ransoft/androidfragments/example/ExampleFragment.kt
@@ -0,0 +1,19 @@
+package com.ransoft.androidfragments.example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.ransoft.androidfragments.R
+
+class ExampleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_example, container, false)
+    }
+}
